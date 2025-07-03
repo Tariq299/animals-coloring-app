e681096170897e3412737d9fe52e68af083d3ebf
Tariq299 <ae628877@gmail.com>
2025-07-03 16:26:29 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,15 @@
-self.addEventListener('install', (event) => {
-  console.log('✅ Service Worker installed');
-  self.skipWaiting();
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("coloring-app-cache").then((cache) =>
+      cache.addAll(["./index.html", "./offline.html"])
+    )
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('✅ Service Worker activated');
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request).then((response) => {
+      return response || caches.match("./offline.html");
+    }))
   );
 });
