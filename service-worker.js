48482a12474694e9fc0017da8c735770e49211f6
Tariq299 <ae628877@gmail.com>
2025-07-04 01:16:28 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,18 @@
-const CACHE_NAME = 'color-app-v1';
+const CACHE_NAME = 'color-app-v2';  // Version bump for updates
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './style.css',
-  './icon-192.png',
-  './icon-512.png',
-  './maskable-192.png',
-  './maskable-512.png',
-  './canvas.js',       // if you separated JS (optional)
-  // Add image URLs used in app:
+  './offline.html',         // Optional if you use offline page
+  './style.css',            // If using external stylesheet
+  './sounds/click.mp3',
+  './sounds/music1.wav',
+  './sounds/music2.wav',
+  './images/icon-192.png',
+  './images/icon-512.png',
+  './images/maskable-192.png',
+  './images/maskable-512.png',
+  // Images used in the app
   'https://i.postimg.cc/RFf6pbPQ/cat.jpg',
   'https://i.postimg.cc/qRyft8Rv/dog.png',
   'https://i.postimg.cc/QNgGhkw6/panda.png',
@@ -22,7 +25,7 @@ self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Install');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log('[ServiceWorker] Caching files');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -32,16 +35,16 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   console.log('[ServiceWorker] Activate');
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
+    caches.keys().then((keyList) =>
+      Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
             console.log('[ServiceWorker] Removing old cache', key);
             return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
@@ -50,7 +53,11 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        return response || fetch(event.request);
+        return response || fetch(event.request).catch(() => {
+          if (event.request.destination === 'document') {
+            return caches.match('./offline.html'); // Optional offline fallback
+          }
+        });
       })
   );
 });
